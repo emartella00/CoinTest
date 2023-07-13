c7309a020e2eade5173e9c4a79a21269b62e1caf
emartella00 <127037344+emartella00@users.noreply.github.com>
2023-07-13 19:23:31 +0200

    Add files via upload

---
+++ contracts/BDT.sol
@@ -0,0 +1,30 @@
+// contracts/BDToken.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "OpenZeppelin/openzeppelin-contracts@4.9.0/contracts/token/ERC20/ERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.9.0/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract BDT is ERC20Burnable {
+    constructor(uint256 initialSupply) ERC20("BToken", "BDT") {
+        _mint(msg.sender, initialSupply);
+    }
+        function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+         function burn(uint256 amount) public virtual override{
+        _burn(_msgSender(), amount);
+    }
+         function burnFrom(address account, uint256 amount) public virtual override {
+        uint256 currentAllowance = allowance(account, _msgSender());
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        unchecked {
+            _approve(account, _msgSender(), currentAllowance - amount);
+        }
+        _burn(account, amount);
+    }
+
+
+
+}
